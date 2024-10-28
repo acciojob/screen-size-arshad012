2e33fe775cb45d887413c9436dc527bb6bbe33ed
arshad012 <forarshad99@gmail.com>
2024-10-29 01:03:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const width = document.getElementById('width');
+const height = document.getElementById('height');
+
+function getWindowSize() {
+	let browser_width = window.innerWidth;
+	let browser_height = window.innerHeight;
+	
+	width.innerHTML = browser_width;
+	height.innerHTML = browser_height;	
+}
+
+window.onload = () => {
+	getWindowSize();
+}
